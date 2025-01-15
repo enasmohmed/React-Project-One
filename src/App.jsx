1e9304d13c5bad9dcd46813ff3555c3e24ebd42e
Enas Mohamed <enasm2477@gmail.com>
2025-01-15 21:32:12 +0200

    edit link navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { About } from "./Components/About/About";
 import { Contact } from "./Components/Contact/Contact";
 import Home from "./Components/Home/Home";
@@ -7,33 +7,34 @@ import Layout from "./Components/Layout/Layout";
 import Portfolio from "./Components/Portfolio/Portfolio";
 
 export default function App() {
+  // إضافة basename هنا للتوافق مع GitHub Pages
+  const router = createBrowserRouter(
+    [
+      {
+        path: "",
+        element: <Layout />,
+        children: [
+          { path: "startFramework", element: <Home /> },
+          { path: "about", element: <About /> },
+          { path: "portfolio", element: <Portfolio /> },
+          { path: "contact", element: <Contact /> },
+          {
+            path: "*",
+            element: (
+              <div className="vh-100 bg-info d-flex justify-content-center align-items-center">
+                <h1>404 Not Found</h1>
+              </div>
+            ),
+          },
+        ],
+      },
+    ],
+    { basename: "/React-Project-One" } // هذا هو المسار الأساسي
+  );
 
-
-
-  const App = () => {
-    return (
-      <Router>
-        <Routes>
-          <Route path="" element={<Layout />}>
-            <Route path="/" element={<Navigate to="/startFramework" />} />
-            <Route path="startFramework" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/portfolio" element={<Portfolio />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route
-              path="/*"
-              element={
-                <div className="vh-100 bg-info d-flex justify-content-center align-items-center">
-                  <h1>404 Not Found</h1>
-                </div>
-              }
-            />
-          </Route>
-        </Routes>
-      </Router>
-    );
-  };
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
-
-
-
