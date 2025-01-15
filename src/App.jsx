6e49312bc25bd17f2bd1cc2ad178da60452ee071
Enas Mohamed <enasm2477@gmail.com>
2025-01-15 20:58:34 +0200

    edit links

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import React from "react";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { About } from "./Components/About/About";
 import { Contact } from "./Components/Contact/Contact";
 import Home from "./Components/Home/Home";
@@ -10,28 +10,28 @@ export default function App() {
 
 
 
-  let router = createBrowserRouter([
-
-
-    {path:"", element:<Layout /> , children:[
-      {path:"startFramework", element:<Home />},
-      {path:"/about", element:<About />},
-      {path:"/portfolio", element:<Portfolio />},
-      {path:"/contact", element:<Contact />},      
-      {path:"/*", element:<div className="vh-100 bg-info d-flex justify-content-center align-items-center">
-        <h1>404 Not Found</h1>
-      </div>},      
-    ]}
-
-    
-  ])
-
-
-  return <>
-    
-    <RouterProvider router={router} />
-
-  </>
+  const App = () => {
+    return (
+      <Router>
+        <Routes>
+          <Route path="" element={<Layout />}>
+            <Route path="startFramework" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route
+              path="/*"
+              element={
+                <div className="vh-100 bg-info d-flex justify-content-center align-items-center">
+                  <h1>404 Not Found</h1>
+                </div>
+              }
+            />
+          </Route>
+        </Routes>
+      </Router>
+    );
+  };
 }
 
 
